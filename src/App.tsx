a520c4d1e5bd2488a6b711a50433f67ad1336c0c
Laura Boemo <lauraboemo@gmail.com>
2022-03-18 23:20:52 -0300

    Adjust

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
 
 function App() {
   return (
-    <Home />
+    <BrowserRouter>
+      <Route path="/">{Home}</Route>
+      <Route path="/rooms/new">{NewRoom}</Route>
+    </BrowserRouter>
   );
 }
 
