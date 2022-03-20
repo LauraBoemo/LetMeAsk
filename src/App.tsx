a25a70849dd18b04445a3a07fb1cea6fea94d94c
Laura Boemo <lauraboemo@gmail.com>
2022-03-20 01:24:49 -0300

    navigation

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 
+import { AuthContextProvider } from './contexts/AuthContext'
+
 function App() {
   return (
     <BrowserRouter>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/rooms/new">
-        <NewRoom />
-      </Route>
+        <AuthContextProvider>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/rooms/new">
+            <NewRoom />
+          </Route>
+        </AuthContextProvider>
     </BrowserRouter>
   );
 }
