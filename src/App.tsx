4f45a9bfef71d0bd7589815b6503f680b03a02c3
Laura Boemo <lauraboemo@gmail.com>
2022-03-24 00:04:53 -0300

    adminroom

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { NewRoom } from "./pages/NewRoom";
 import { Room } from './pages/Room';
 
 import { AuthContextProvider } from './contexts/AuthContext'
+import { AdminRoom } from './pages/AdminRoom';
 
 function App() {
   return (
@@ -19,6 +20,9 @@ function App() {
             </Route>
             <Route path="/rooms/:id">
               <Room />
+            </Route>
+            <Route path="/admin/rooms/:id">
+              <AdminRoom />
             </Route>
           </Switch>
         </AuthContextProvider>
